267947622846de6d8857579f4c8e9811a16ee405
user <user@GRIGORYWORKPC>
2021-12-02 16:23:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+/* Написать программу которая будет определять
+ * является ли слово или цифра палиндромом */
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Проверить слово, является ли палиндромом" << endl;
+	string text;
+	cin >> text;
+	if (string(text.begin(), text.end()) == string(text.rbegin(), text.rend()))
+		cout << "Слово палиндром";
+	else
+		cout << "Не является палиндромом";
+}
